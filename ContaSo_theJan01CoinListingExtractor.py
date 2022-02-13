a9bad26ae256b3d71965ba60794e02fedd7e1ab0
IvanP <ivan.perletti@gmmspa.com>
2022-02-13 15:10:57 +0100

    coinmarket cap wweb scaroing with no API

--- ContaSo_theJan01CoinListingExtractor.py
+++ ContaSo_theJan01CoinListingExtractor.py
@@ -13,8 +13,8 @@ import pandas as pd
 #pd.options.display.max_columns = None
 #pd.options.display.max_rows = None
 # Retrieve the top 5000 coins based on market cap
-
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/historical/20210101/'
+url = 'https://pro-api.coinmarketcap.com/v1/exchange/quotes/historical/20210101'
+# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/historical/20210101/'
 parameters = {
   'start':'1',
   'limit':'5000',
@@ -38,19 +38,31 @@ except (ConnectionError, Timeout, TooManyRedirects) as e:
 
 
 
+import time
+import requests
 
 
+def get_timestamp(datetime: str):
+    return int(time.mktime(time.strptime(datetime, '%Y-%m-%d %H:%M:%S')))
 
-header = ['name', 'area', 'country_code2', 'country_code3']
-data = ['Afghanistan', 652090, 'AF', 'AFG']
+def getCryptoQuotes(symbol: str, start_date: str, end_date: str):
+    start = get_timestamp(start_date)
+    end = get_timestamp(end_date)
+    url = f'https://web-api.coinmarketcap.com/v1/cryptocurrency/ohlcv/historical?symbol={symbol}&convert=USD&time_start={start}&time_end={end}'
+    return requests.get(url).json()
 
-with open('countries.csv', 'w', encoding='UTF8',newline='') as f:
-    writer = csv.writer(f)
 
-    # write the header
-    writer.writerow(header)
+crypto = [ 'ETH', 'BNB' , 'XRP' ]
 
-    writer.writerow(coindf)
+for symbol in crypto:
+    data = getCryptoQuotes(symbol,
+                      start_date='2021-01-01 00:00:00',
+                      end_date='2021-01-02 00:00:00')
 
+    # making A LOT of assumptions here, hopefully the keys don't change in the future
+    data_flat = [quote['quote']['USD'] for quote in data['data']['quotes']]
+    df = pd.DataFrame(data_flat)
     
-# -*- coding: utf-8 -*-
+    print(df)
+
+
