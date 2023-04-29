55dcac6477d1865d22d560380e007febc9ac5bc3
IvanP <ivan.perletti@gmmspa.com>
2023-04-29 06:33:42 +0200

    fix etf bollinger band

---
+++ Stock_Analysis/03_Trading/ZamoTrader_ETF_01.py
@@ -0,0 +1,287 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 13 06:49:45 2021
+
+@author: perletti
+"""
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator, FormatStrFormatter
+#import mplfinance as mpf 
+# import seaborn as sns
+import datetime as dt
+import dateutil.relativedelta
+
+#handle csv files
+import numpy as np
+#used for scientific calculation
+from sklearn.svm import SVR
+#used for ml
+import matplotlib.pyplot as matplt
+
+
+# %matplotlib qt
+plt.close('all')
+currency = "USD"
+metric = "Open"
+dateEnd =  dt.datetime.today().date()
+dateStart = dateEnd - dateutil.relativedelta.relativedelta(years=3)
+
+today = dateEnd
+
+
+
+
+crypto = [ 
+    # [rsk2] --------------------------
+    # 'DPW.DE', #Deutsche Post AG (
+    # 'ITT', #ITT Motion Technologies produce e vende pastiglie per freni e materiali di attrito
+    # 'ACN', #'Accenture plc
+    # 'RL', #Ralph Lauren Corporation 
+    # 'TGYM.MI', # TecgnoGym
+    # 'SL.MI' , # San Lorenzo  YACT
+    'MSFT' ,#Microsoft **
+    # 'MELE.BR', # Melexis
+    'WCN', #Waste Connections, Inc.
+    'AD.AS', #Koninklijke Ahold Delhaize 
+    'RFG.MI', # Racing Force S.P.A. Formula 1 Sicruezza
+    # [rsk3] --------------------------
+    # 'TISG.MI', # The Italian Sea Group S.p.A. (YACT)
+    # 'AGS.BR' ,  # ageas SA/NV (holding assicurativa) Belgio, UK, EUR ASIA
+    # 'BELL.SW', #Bell Food Group AG (carne )
+    # 'MDT',# Medtronic plc (MDT)
+    # 'DIS', # The Walt Disney Company (DIS)z
+    # 'ENGI.PA', # Engie
+    # 'VFC' , #V.F. Corporation moda -  Timb!erland, Eastpack, Jansport
+    # 'EAPI.PA' , #Euroapi S.A.  principi attivi per farmaci B12 A, C
+    #...  ETF   --------------------------
+    'HMWD.L', # HSBC MSCI World UCITS ETF
+    # # 'SUAS.L', # az usa 5/15% [$] costa tanto
+                # 'SPXD.SW',# az usa S&P500 (15/15%) [$]
+                # 'CANA.SW', # AZ CANADA 5% [$]
+                # 'XCHA.L', # az china CHINA 10% [$]
+                # 'AUSAUW.SW', # az AUS 5% [$]
+                # 'ISFA.AS', # az GBP 5% [$]
+                # 'SW2CHB.SW', # az svizz 10% [$]
+                # 'XDJP.DE', # japan 10% [$]
+    # 'STW.PA', #SPDR MSCI Europe Health Care UCITS ETF (STW.PA)
+          ]
+colnames = []
+
+first = True
+
+for ticker in crypto:
+    strCsvFile = f'{ticker}-{today}.csv'
+    
+    try:
+        data_Raw = pd.read_csv(strCsvFile, parse_dates=[0], dayfirst=True, index_col = 'Date')
+    except OSError:
+        # data_Raw = web.DataReader(f'{ticker}', "yahoo", dateStart, dateEnd)
+        data_Raw = yf.download(f'{ticker}', 
+                      start=dateStart, 
+                      end=dateEnd, 
+                      progress=False,interval = "1d") #/!\ other than 5d samples can fail
+        data_Raw.to_csv(strCsvFile)
+        
+        
+    data = pd.DataFrame() # combined data frame
+            # Divide the DataFrame1 elements by the elements of DataFrame2
+
+    data['Open']      = data_Raw['Open']
+    data['High']      = data_Raw['High']
+    data['Low']       = data_Raw['Low']
+    data['Adj Close'] = data_Raw['Adj Close']
+    data['Volume'] = data_Raw['Volume']
+        
+    data_Raw.dropna(inplace = True) # not a number values
+    #data2 =  yf.download('BTC',dateStart,interval="1h")
+    for ii in range(2):
+        strTitle = ' 5Days'
+        data = np.log(  data_Raw.resample('5d').agg({'Open': 'first', 
+                                      'High': 'max', 
+                                      'Low': 'min', 
+                                      'Adj Close': 'mean',
+                                      'Volume': 'sum'}))
+
+        if ii>0:
+            strTitle = ' 20Days'
+            data = data_Raw.resample('20d').agg({'Open': 'first', 
+                                          'High': 'max', 
+                                          'Low': 'min', 
+                                          'Adj Close': 'mean',
+                                          'Volume': 'sum'})
+        
+
+
+        
+        delta = data['Adj Close'].diff(1)
+        delta.dropna(inplace = True) # not a number values
+        positive = delta.copy()
+        negative = delta.copy()
+        
+        positive  [positive  < 0] = 0
+        negative [negative > 0] = 0
+        
+        
+        # RSI calculation
+        def calculate_RSI(positive, negative, days=14):
+                
+            average_gain = positive.rolling(window=days).mean()
+            average_loss = abs ( negative.rolling(window=days).mean() )
+            relative_strength = average_gain / average_loss
+            RSI = 100.0 / (1.0 + relative_strength)
+            return RSI
+        
+        RSI_14 = calculate_RSI(positive, negative, 14)
+        # RSI_21 = calculate_RSI(positive, negative, 21)
+        
+        # RSI calculation
+        
+        lDayK = 14
+        lDayD = 5 # mobile average
+        v14Low  = data['Low'].rolling(lDayK).min()
+        v14High = data['High'].rolling(lDayK).max()
+        vK = ( data['Adj Close'] - v14Low ) * 100.0 / ( v14High - v14Low ) 
+        vD = vK.rolling(lDayD).mean()
+        
+       
+        
+        sEMA = (26,12,9)
+        longEMA = data['Adj Close'].ewm(span=sEMA[0], adjust=False, min_periods=sEMA[0]).mean()
+        shortEMA = data['Adj Close'].ewm(span=sEMA[1], adjust=False, min_periods=sEMA[1]).mean()
+
+        
+        mac = pd.DataFrame() # combined data frame
+        mac['D'] = (  shortEMA - longEMA )
+        mac['Signal'] = mac['D'].ewm(span=sEMA[2], adjust=False,min_periods=9).mean()
+        mac['Diff'] =  3*( mac['D'] - mac['Signal'] )
+        mac['Lapl'] = 3* ( mac['Diff'].rolling(window=1).mean().diff(1))
+        
+        combined = pd.DataFrame() # combined data frame
+        combined['Adj Close'] = data ['Adj Close']
+        combined['RSI_14'] = (100.0-RSI_14)
+        combined['%K'] = vK
+        combined['%D'] = vD
+        
+        
+        period = 20
+
+        #Calculate 20-ma and std dev
+        combined['ma_20'] = data ['Adj Close'].rolling(period).median()
+        combined['std'] = data ['Adj Close'].rolling(period).std()
+        # combined['std'] = np.abs(data ['Adj Close'].quantile(0.92) - data ['Adj Close'].quantile(0.08))*0.1667
+        #Calculate bollinger bands using std dev
+        # combined['upper_bollinger'] = combined['ma_20'] + (2 * combined['std'])
+        # combined['lower_bollinger'] = combined['ma_20'] - (2 * combined['std'])
+        
+        combined['upper_bollinger'] = data ['Adj Close'].rolling(period).quantile(0.90)
+        combined['lower_bollinger'] = data ['Adj Close'].rolling(period).quantile(0.10)
+        
+        
+        obv = (np.sign(data['Adj Close'].diff()) * data['Volume']).fillna(0).cumsum()
+        obv = 100 * obv / obv.max()
+        
+        plt.figure(figsize = (12,8))
+        #figA.canvas.mpl_connect('pick_event', DataCursor(plt.gca()))
+
+
+        combined = combined.iloc[-80: , :] 
+        mac = mac.iloc[-80: , :] 
+        obv = obv.iloc[-80:]
+        # vBuy =  ( (combined['%D']<30) & (mac['Lapl']>0)).astype(float)
+        vBuy =  ( (mac['Diff']<0) & (mac['Lapl']>0)).astype(float)
+        vBuy  = vBuy + ( combined['%D'] < 30).astype(float)
+        vBuy = vBuy * mac['Diff'].max() * 0.5
+        
+        # vSell =  ( (combined['%D']>70)  & (mac['Lapl']<0)).astype(float)
+        vSell =  ( (mac['Diff']>0)  & (mac['Lapl']<0)).astype(float)
+        vSell = vSell + ( combined['%D'] > 70).astype(float)
+        vSell = vSell * mac['Diff'].max() * 0.5
+        
+        
+        ax1 = plt.subplot(311)
+
+        # ax1.plot(obv.index, obv, color = 'yellow')
+
+        ax1.plot(combined.index, combined['ma_20'], label = '20 ma')
+        ax1.fill_between(combined.index, combined['upper_bollinger'], combined['lower_bollinger'], label = 'Bollinger Bands', color='darkgrey')
+        line1, = ax1.plot(combined.index, combined['Adj Close'], color = 'red')
+
+        ax1.set_title(ticker+strTitle, color='white')
+        ax1.grid(True, color = '#555555')
+        ax1.set_axisbelow(True)
+        ax1.set_facecolor('black')
+        ax1.figure.set_facecolor('#121212')
+        ax1.tick_params(axis = 'x', colors='white')
+        ax1.tick_params(axis = 'y', colors='white')
+        
+        
+                
+        ax2 = plt.subplot(312, sharex = ax1)
+        # ax2.plot(mac.index, mac['D'], label =  'MACD', color = 'green')
+        # ax2.plot(mac.index, mac['Signal'], label =  'Signal', color = 'red')
+        ax2.plot(mac.index, mac['Diff'], label =  'Delta', color = 'grey')
+        ax2.plot(mac.index, vBuy, label =  'buy', color = 'pink')
+        ax2.plot(mac.index, vSell, label =  'sell', color = 'cyan')
+        ax2.plot(mac.index, mac['Lapl'], label =  'sell', color = 'white')
+        ax2.axhline(0,linestyle='--', alpha =0.5, color = '#CCccCC')
+        ax2.grid(True, color = '#555555')
+        ax2.set_axisbelow(True)
+        ax2.set_facecolor('black')
+        ax2.figure.set_facecolor('#121212')
+        ax2.tick_params(axis = 'x', colors='white')
+        ax2.tick_params(axis = 'y', colors='white')
+
+
+
+        ax3 = plt.subplot(313, sharex = ax1)
+        # line2, = ax3.plot(combined.index, combined['RSI_14'], color = 'lightgray')
+        ax3.plot(combined.index, combined['%K'], color = 'magenta')
+        ax3.plot(combined.index, combined['%D'], color = 'cyan')
+        
+        
+        ax3.axhline(0, linestyle='--', alpha =0.5, color = '#FF0000')
+        ax3.axhline(10,linestyle='--', alpha =0.5, color = '#FFaa00')
+        ax3.axhline(20,linestyle='--', alpha =0.5, color = '#00FF00')
+        ax3.axhline(30,linestyle='--', alpha =0.5, color = '#CCccCC')
+        
+        ax3.axhline(100,linestyle='--', alpha =0.5, color = '#FF0000')
+        ax3.axhline(90 ,linestyle='--', alpha =0.5, color = '#FFaa00')
+        ax3.axhline(80 ,linestyle='--', alpha =0.5, color = '#00FF00')
+        ax3.axhline(70 ,linestyle='--', alpha =0.5, color = '#CCccCC')
+        
+        
+        ax3.set_title("RSI", color='white')
+        ax3.grid(False)
+        ax3.set_axisbelow(False)
+        ax3.set_facecolor('black')
+        ax3.tick_params(axis = 'x', colors='white')
+        ax3.tick_params(axis = 'y', colors='white')
+        
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
